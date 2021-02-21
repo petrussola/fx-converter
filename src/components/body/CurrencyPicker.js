2f4777b975e8e536885c765df47b7012cf58e115
Pere Solà Claver <pere.solaclaver@gmail.com>
2021-02-21 21:07:09 +0000

    Set state for destination currency when currency is selected

--- src/components/body/CurrencyPicker.js
+++ src/components/body/CurrencyPicker.js
@@ -22,11 +22,16 @@ export default function CurrencyPicker({ defaultCurrency, name }) {
 
   const selectCurrency = async (e) => {
     try {
-      // call helper function that will call the fx api and return new selected currency state
-      const newSelectedCurrency = await grabFx(e.target.value);
       // if selector is base currency, set selected base currency state, which comes already shaped as {iso: {{base currency}}, fx: {fx to other symbols}}
       if (name === "baseCurrency") {
+        // call helper function that will call the fx api and return new selected currency state
+        const newSelectedCurrency = await grabFx(e.target.value);
         setSelectedBaseCurrency(newSelectedCurrency);
+      } else {
+        setSelectedDestinationCurrency({
+          ...selectedDestinationCurrency,
+          iso: e.target.value,
+        });
       }
     } catch (error) {
       console.log(error.message);
